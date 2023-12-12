675c8fb61263049bc26884f274222c75b633e146
DFCMK <dkalnoky@gmail.com>
2023-12-12 14:34:36 +0000

    added __init__ function to initialize the game

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import random
 import time
 
-grid = [[]]
-grid_size = 10
-number_of_ships = 2
-bullets_left = 50
-game_over = False
-num_of_ships_sunk = 0
-ship_positions = 0
-alphabeth = "ABCDEFGHIJKLMNOPQRSTUVW"
+class Battleship:
+   def __init__(self, grid_size=10, num_of_ships=2, bullets=50):
+    """
+    Initialize the game with a grid of a given size as Gameboard with a number of ships, and a number of bullets.
+    """
+       self.grid_size = grid_size
+       self.num_of_ships = num_of_ships
+       self.bullets = bullets
+       self.grid = [['.' for _ in range(grid_size)] for _ in range(grid_size)]
+       self.ship_positions = []
+       self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+       self.create_grid()
 
