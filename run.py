7b1693655225a7d165b82782966490101ff08ff1
DFCMK <dkalnoky@gmail.com>
2023-12-12 14:14:51 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+
+grid = [[]]
+grid_size = 10
+number_of_ships = 2
+bullets_left = 50
+game_over = False
+num_of_ships_sunk = 0
+ship_positions = 0
+alphabeth = "ABCDEFGHIJKLMNOPQRSTUVW"
+
