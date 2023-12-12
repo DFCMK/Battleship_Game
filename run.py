18f5b6fb97753dd3315ecb8c39c8fd7bf04733d1
DFCMK <dkalnoky@gmail.com>
2023-12-12 18:32:10 +0000

    place_ship function handle random positioning

--- run.py
+++ run.py
@@ -20,3 +20,34 @@ def random_col():
     '''
     return random.randint(0, GRID_SIZE, -1)
 
+def place_ship():
+    '''
+    Handle positioning of a single ship.
+    '''
+
+    row = random_row()
+    col = random_col()
+
+    # Randomly choose vertical or horizontal orientation
+    is_vertical = random.choice([True, False])
+
+    if is_vertical:
+        if row + size > GRID_SIZE:
+            return False
+
+        for i in range(size):
+            grid[row + i][col] = ship[0]
+    else:
+        if col + size > GRID_SIZE:
+            return False
+
+        for i in range(size):
+            grid[row][col + i] = ship[0]
+
+    return True
+
+    for ship, size in ship.items():
+        while True:
+            placed = place_ship(ship, size, grid)
+        if placed:
+            break
